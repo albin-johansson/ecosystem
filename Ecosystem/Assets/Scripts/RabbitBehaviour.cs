08bbfd93c068e79bed25260bfdaebd688f8adc48
EdwinHolst <43250892+EdwinHolst@users.noreply.github.com>
2021-01-28 14:48:27 +0100

    Add Hunger and Thirst to Rabbit
    Add Hunger and Thirst

--- Ecosystem/Assets/Scripts/RabbitBehaviour.cs
+++ Ecosystem/Assets/Scripts/RabbitBehaviour.cs
@@ -7,46 +7,82 @@ public class RabbitBehaviour : MonoBehaviour
 {
     public Camera cam;
     public UnityEngine.AI.NavMeshAgent agent;
-    public GameObject food;
-    public LayerMask whatIsGround, whatIsFood;
-    public float hunger;
+    public GameObject food, water;
+    public LayerMask whatIsGround, whatIsFood, whatIsWater;
+    public double hunger, thirst;
+    public Vector3 walkPoint;
+    
+    
 
-    public bool foodInSightRange;
+    public bool foodInSightRange, waterInSightRange;
     public float sightRange;
 
     // Start is called before the first frame update
     void Start()
     {
+        hunger = 0;
+        thirst = 0;
+    }
+
+    
+    private void SearchFood(){
+        foodInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsFood);
+        if(foodInSightRange){
+            agent.SetDestination(food.transform.position);
+        } else {
+            Search();
+        }
         
     }
 
-    // Update is called once per frame
-    private void ChaseFood(){
-        agent.SetDestination(food.transform.position);
+    private void SearchWater(){
+        waterInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsWater);
+        if(waterInSightRange){
+            agent.SetDestination(water.transform.position);
+        } else {
+            Search();
+        }
+         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        foodInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsFood);
+    private void Search(){
+        if(!agent.hasPath){
+            float randomZ = Random.Range(-5f, 5f);
+            float randomX= Random.Range(-5f, 5f);
 
-        if(foodInSightRange) ChaseFood();
+            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-        if (Input.GetMouseButtonDown(0)){
-            Ray ray =cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if(Physics.Raycast(ray, out hit)){
-                agent.SetDestination(hit.point);
+            if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)){
+                agent.SetDestination(walkPoint);
             }
         }
     }
+
+
+    
+    void Update()
+    {   
+        hunger += 0.1*Time.deltaTime;
+        thirst += 0.2*Time.deltaTime;
+        if(hunger > thirst){
+            SearchFood();
+        } else {
+            SearchWater();
+        }
+    }
+
     private void OnTriggerEnter(Collider other){
          if(other.tag =="Food"){
-             Debug.Log("Hit detected");
+             Debug.Log("Food detected");
               Destroy(other);
          }
          
+          if(other.tag =="Water"){
+             Debug.Log("Water detected");
+             thirst = 0;
+         }
+
+        
 
     }
 
