1d6f9ba3eaf8f41cf825154254ef056325bf2737
Albin Johansson <albinoscar99@hotmail.com>
2021-02-01 20:58:40 +0100

    Fix hardcoded resources in RabbitBehaviour
    RabbitBehaviour no longer requires hardcoded food and game objects. This structure should be seen as somewhat experimental and open for change and/or improvement, I tried to make it as intuitive as possible.

--- Ecosystem/Assets/Scripts/RabbitBehaviour.cs
+++ Ecosystem/Assets/Scripts/RabbitBehaviour.cs
@@ -3,107 +3,37 @@ using UnityEngine.AI;
 
 public sealed class RabbitBehaviour : MonoBehaviour
 {
-  public NavMeshAgent navAgent;
-  public GameObject food; // FIXME should not be hardcoded
-  public GameObject water; // FIXME should not be hardcoded
+  [SerializeField] private NavMeshAgent navAgent;
 
-  [Tooltip("What is considered to be the ground")]
-  public LayerMask groundMask;
+  [SerializeField, Tooltip("What is considered to be the ground")]
+  private LayerMask groundMask;
 
-  [Tooltip("What is considered to be food")]
-  public LayerMask foodMask;
+  private Transform _transform;
 
-  [Tooltip("What is considered to be water")]
-  public LayerMask waterMask;
-
-  public float sightRange = 5;
-
-  private double _hunger;
-  private double _thirst;
-
-  private void TargetRandomDestination()
+  private void Start()
   {
-    if (navAgent.hasPath)
-    {
-      return;
-    }
-
-    var randomX = Random.Range(-5f, 5f);
-    var randomZ = Random.Range(-5f, 5f);
-
-    var position = transform.position;
-    var destination = new Vector3(position.x + randomX, position.y, position.z + randomZ);
-
-    if (Physics.Raycast(destination, -transform.up, 2f, groundMask))
-    {
-      navAgent.SetDestination(destination);
-    }
-  }
-
-  private bool InSight(LayerMask mask)
-  {
-    return Physics.CheckSphere(transform.position, sightRange, mask);
-  }
-
-  private bool IsFoodInSight()
-  {
-    return InSight(foodMask);
-  }
-
-  private bool IsWaterInSight()
-  {
-    return InSight(waterMask);
-  }
-
-  private void SearchForFood()
-  {
-    if (IsFoodInSight())
-    {
-      navAgent.SetDestination(food.transform.position);
-    }
-    else
-    {
-      TargetRandomDestination();
-    }
-  }
-
-  private void SearchForWater()
-  {
-    if (IsWaterInSight())
-    {
-      navAgent.SetDestination(water.transform.position);
-    }
-    else
-    {
-      TargetRandomDestination();
-    }
+    _transform = transform;
   }
 
   private void Update()
   {
-    _hunger += 0.1 * Time.deltaTime;
-    _thirst += 0.2 * Time.deltaTime;
-    if (_hunger > _thirst)
+    if (!navAgent.hasPath)
     {
-      SearchForFood();
-    }
-    else
-    {
-      SearchForWater();
+      TargetRandomDestination();
     }
   }
 
-  private void OnTriggerEnter(Collider other)
+  private void TargetRandomDestination()
   {
-    if (other.CompareTag("Food"))
-    {
-      Destroy(other);
-      _hunger = 0;
-    }
+    var randomX = Random.Range(-5f, 5f);
+    var randomZ = Random.Range(-5f, 5f);
 
-    if (other.CompareTag("Water"))
+    var position = _transform.position;
+    var destination = new Vector3(position.x + randomX, position.y, position.z + randomZ);
+
+    if (Physics.Raycast(destination, -_transform.up, 2f, groundMask))
     {
-      _thirst = 0;
+      navAgent.SetDestination(destination);
     }
   }
 }
