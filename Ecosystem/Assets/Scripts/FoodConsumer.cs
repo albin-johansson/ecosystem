163d0368dacb3a36fb91877555fdcc206b24c6a3
Albin Johansson <albinoscar99@hotmail.com>
2021-02-01 20:56:25 +0100

    Add FoodConsumer class

---
+++ Ecosystem/Assets/Scripts/FoodConsumer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public sealed class FoodConsumer : MonoBehaviour
+{
+  [SerializeField] private double rate = 0.02;
+  [SerializeField] private double threshold = 0.1;
+
+  private double Hunger { get; set; }
+
+  private void Update()
+  {
+    Hunger += rate * Time.deltaTime;
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.GetComponent<Food>() != null)
+    {
+      Destroy(other);
+      Hunger = 0;
+    }
+  }
+
+  internal bool IsHungry()
+  {
+    return Hunger > threshold;
+  }
+}
