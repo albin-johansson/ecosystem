05c638caaf0ac5796e34e5cc5bb2dfbf83789870
Braxen <oliver.k@live.se>
2021-02-03 09:16:10 +0100

    Add: A spawn on left click script that takes a prefab and spawns it on the mouse position.

---
+++ Ecosystem/Assets/Scripts/OnClickSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                SpawnAtPosition(hitInfo.point);
+            }
+        } 
+    }
+    private void SpawnAtPosition(Vector3 spawnPosition)
+    {
+        GameObject food = Instantiate(prefab, spawnPosition, Quaternion.identity);
+    }
+
+}
