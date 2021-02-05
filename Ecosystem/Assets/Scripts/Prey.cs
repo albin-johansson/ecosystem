10b8759965dffbbca784729a15b329575c24a9cf
Karl Öqvist <54830421+karloq@users.noreply.github.com>
2021-02-05 12:33:06 +0100

    Update Prey.cs

--- Ecosystem/Assets/Scripts/Prey.cs
+++ Ecosystem/Assets/Scripts/Prey.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-// A tag component used to denote a game object as a food resource
+// A tag component used to denote a game object as a prey resource
 public sealed class Prey : MonoBehaviour
 {
 }
