c85380e3582cbddea64ffaca3d3f6ab366e325e7
Braxen <oliver.k@live.se>
2021-02-08 09:34:53 +0100

    Add: Spawn scripts that use rays and only spawn on"ground" tags

---
+++ Ecosystem/Assets/Scripts/SpawnWithRays.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWithRays : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Terrain terrain;
+    [SerializeField] private float rate;
+    private float elapsedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (rate < elapsedTime)
+        {
+            elapsedTime = 0;
+            RaycastHit hit;
+            var terrainData = terrain.terrainData;
+            float xPos = Random.Range(-terrainData.bounds.extents.x, terrainData.bounds.extents.x);
+            float zPos = Random.Range(-terrainData.bounds.extents.z, terrainData.bounds.extents.z);
+            
+            
+            Vector3 position = terrainData.bounds.center + new Vector3(xPos, 0, zPos);
+            float height = terrain.SampleHeight(terrainData.bounds.center + new Vector3(xPos, 0, zPos)) + 10;
+            
+            if (Physics.Raycast (position + new Vector3(0, height, 0), Vector3.down, out hit, 200.0f)) {
+                if (hit.transform.CompareTag("Ground"))
+                {
+                    Instantiate (prefab, hit.point, Quaternion.identity);
+                }
+            } else {
+                Debug.Log ("there seems to be no ground at this position");
+            }
+        }
+    }
+}
---
+++ Ecosystem/Assets/Scripts/SpawnWithRaysInRadius.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWithRaysInRadius : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private float rate;
+    [SerializeField] private float radius;
+    private float elapsedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        elapsedTime += Time.deltaTime;
+        if (rate < elapsedTime)
+        {
+            elapsedTime = 0;
+            float distance = Random.Range(0, radius);
+            Vector2 dir = Random.insideUnitCircle;
+            Vector3 position = transform.position + distance * new Vector3(dir.x, 0, dir.y);
+            
+            RaycastHit hit;
+            
+            if (Physics.Raycast (position + new Vector3(0, transform.position.y + 5, 0), Vector3.down, out hit, 200.0f)) {
+                if (hit.transform.CompareTag("Ground"))
+                {
+                    Instantiate (prefab, hit.point, Quaternion.identity);
+                }
+            }
+        }
+        
+    }
+}
