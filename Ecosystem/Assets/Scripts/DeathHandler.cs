e05309b1465c2f9e41aeff26cd96e69af837ea1e
mattiasHeinl <heinl@student.chalmers.se>
2021-02-11 20:25:46 +0100

    Minor fix

--- Ecosystem/Assets/Scripts/DeathHandler.cs
+++ Ecosystem/Assets/Scripts/DeathHandler.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Handles killing the game object in a more 
+/// Handles killing the game object.
 /// </summary>
-public class DeathHandler : MonoBehaviour
+public sealed class DeathHandler : MonoBehaviour
 {
   void DestroyObjectDelayed()
   {
     //TODO: make the time depend on the death animation
-    Destroy(gameObject.gameObject);
+    Destroy(gameObject.gameObject, 5);
   }
 
   public void KillMe(CauseOfDeath cause)
