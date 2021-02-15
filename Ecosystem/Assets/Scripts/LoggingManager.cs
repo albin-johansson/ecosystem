ef24dd33ffbefc48b3beaf0dadb0107dd64cf835
mattiasHeinl <heinl@student.chalmers.se>
2021-02-15 18:56:22 +0100

    First pass, testing how stuff works

---
+++ Ecosystem/Assets/Scripts/LoggingManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class LoggingManager : MonoBehaviour
+{
+  private List<GameObject> trackedObjects = new List<GameObject>();
+
+  // called zero
+  private void Awake()
+  {
+    Debug.Log("Awake");
+  }
+
+  // called first
+  private void OnEnable()
+  {
+    foreach (var gameObject
+      in SceneManager.GetActiveScene().GetRootGameObjects())
+    {
+      if (false)
+      {
+        trackedObjects.Add(gameObject);
+      }
+    }
+  }
+
+  // called second
+  private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+  {
+  }
+
+  // called third
+  private void Start()
+  {
+    Debug.Log("Start");
+    SceneManager.GetActiveScene().GetRootGameObjects();
+  }
+
+  void OnDisable()
+  {
+    int i = 0;
+    // Write to file/json/store data
+
+    /*
+    Debug.Log("OnDisable");
+    SceneManager.sceneLoaded -= OnSceneLoaded;
+    */
+  }
+
+  private void UpdateData()
+  {
+    var time = AnalyticsSessionInfo.sessionElapsedTime;
+  }
+}
