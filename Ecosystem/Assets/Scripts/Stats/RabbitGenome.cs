9146f284bb7dd0bf36a9aa5719222c7a942251d1
mattiasHeinl <heinl@student.chalmers.se>
2021-02-17 00:31:55 +0100

    Minor fix

--- Ecosystem/Assets/Scripts/Stats/RabbitGenome.cs
+++ Ecosystem/Assets/Scripts/Stats/RabbitGenome.cs
@@ -8,14 +8,13 @@ public sealed class RabbitGenome : Genome
 
   public RabbitGenome(double mutateChance) : base(mutateChance)
   {
-    Genes = new Dictionary<GeneType, Gene>();
-    Genes.Add(GeneType.HungerRate, new Gene(1, 0.5, 10));
-    Genes.Add(GeneType.HungerThreshold, new Gene(5, 0, 10));
-    Genes.Add(GeneType.ThirstRate, new Gene(1, 0.5, 10));
-    Genes.Add(GeneType.ThirstThreshold, new Gene(5, 0, 10));
-    Genes.Add(GeneType.Vision, new Gene(25, 1, 50));
-    Genes.Add(GeneType.SpeedFactor, new Gene(1.5, 1, 2));
-    Genes.Add(GeneType.SizeFactor, new Gene(0.5, 0.1, 1));
-    Genes.Add(GeneType.DesirabilityScore, new Gene(1, 1, 10));
+    Genes[GeneType.HungerRate] = new Gene(1, 0.5, 10);
+    Genes[GeneType.HungerThreshold] = new Gene(5, 0, 10);
+    Genes[GeneType.ThirstRate] = new Gene(1, 0.5, 10);
+    Genes[GeneType.ThirstThreshold]= new Gene(5, 0, 10);
+    Genes[GeneType.Vision] = new Gene(25, 1, 50);
+    Genes[GeneType.SpeedFactor] = new Gene(1.5, 1, 2);
+    Genes[GeneType.SizeFactor] = new Gene(0.5, 0.1, 1);
+    Genes[GeneType.DesirabilityScore] =  new Gene(1, 1, 10);
   }
 }
