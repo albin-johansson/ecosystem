0d46b551d3d283e4816f1c697713131c2bf3d8b0
mattiasHeinl <heinl@student.chalmers.se>
2021-02-24 20:54:37 +0100

    Testing how shaders work

---
+++ Ecosystem/Assets/Scripts/CameraShader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Ecosystem
+{
+  public class CameraShader : MonoBehaviour
+  {
+    [SerializeField] private Camera camera;
+    [SerializeField] private Shader shader;
+
+    public void Start()
+    {
+      camera.RenderWithShader(shader, "");
+      Debug.Log("After RenderWithShader");
+      //Camera.current.RenderWithShader(Shader.Find("Toon"), "");
+    }
+  }
+}
