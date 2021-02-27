1521a696833f40a7bc9c3d965fbbe45bbcca8efb
Albin Johansson <albinoscar99@hotmail.com>
2021-02-27 17:34:18 +0100

    Add a function for producing a stackplot on population sizes

--- Visualisation/scripts/animalpopulation.py
+++ Visualisation/scripts/animalpopulation.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from logdata import *
 
 
-def visualise_animal_populations(data: LogData, directory: Path):
+def visualise_animal_populations_standard(data: LogData, directory: Path):
   """
   Produces a plot of how the predator and prey populations changed over the course
   of the simulation.
@@ -28,3 +28,71 @@ def visualise_animal_populations(data: LogData, directory: Path):
 
   plot.savefig(directory / Path("animal_populations.png"))
   plot.close()
+
+
+def visualise_animal_populations_stackplot(data: LogData, directory: Path):
+  """
+  Produces a stackplot of how the predator and prey populations changed over the course
+  of the simulation.
+  :param data: the simulation data to read from.
+  :param directory:
+  :return: the directory to which the plot will be saved.
+  """
+
+  prey_count = data.initial_prey_count()
+  predator_count = data.initial_predator_count()
+
+  times: list[TimePoint] = []
+  prey_history: list[Amount] = []
+  predator_history: list[Amount] = []
+
+  times.append(0)
+  prey_history.append(prey_count)
+  predator_history.append(predator_count)
+
+  for death in data.deaths():
+    time: TimePoint = death["time"] / 1_000
+    times.append(time)
+
+    if death["tag"] == "Predator":
+      predator_count = predator_count - 1
+
+    elif death["tag"] == "Prey":
+      prey_count = prey_count - 1
+
+    prey_history.append(prey_count)
+    predator_history.append(predator_count)
+
+  times.append(data.duration_secs())
+  predator_history.append(predator_count)
+  prey_history.append(prey_count)
+
+  population_by_tag = {
+    "Prey": prey_history,
+    "Predators": predator_history
+  }
+
+  figure, axes = plot.subplots()
+  axes.stackplot(times, population_by_tag.values(),
+                 labels=population_by_tag.keys())
+  axes.legend(loc='upper left')
+  axes.set_title("Population size changes")
+  axes.set_xlabel("Time (seconds)")
+  axes.set_ylabel("Population size")
+  axes.set_xlim(0, data.duration_secs())
+  axes.set_ylim(0, data.initial_total_alive_count() + 20)
+
+  plot.savefig(directory / Path("animal_populations_stackplot.png"))
+  plot.close()
+
+
+def visualise_animal_populations(data: LogData, directory: Path):
+  """
+  Produces two plots of how the predator and prey populations changed over the course
+  of the simulation.
+
+  :param data: the simulation data to read from.
+  :param directory: the directory to which the plot will be saved.
+  """
+  visualise_animal_populations_standard(data, directory)
+  visualise_animal_populations_stackplot(data, directory)
