e89ba0d5795d961c8a055918ff0b8db073a32190
SebastianHolm <54844314+SebastianHolm@users.noreply.github.com>
2021-03-01 15:53:07 +0100

    Added a day and night cycle to the main menu

--- Ecosystem/Assets/Scenes/MainMenuScene/LightRotation.cs
+++ Ecosystem/Assets/Scenes/MainMenuScene/LightRotation.cs
@@ -4,27 +4,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LightRotation : MonoBehaviour
 {
-  private DirectionalLight sun;
-  private float ypos;
-  private float zpos;
-  private float wpos;
+  [SerializeField] private Light sun;
+  [SerializeField] private Camera cam;
+  [SerializeField] private ParticleSystem particleSystem;
+  private bool isNight;
+  private float rotation;
+  private float intesityTemp;
+  
 
   private void Start()
   {
-    ypos = sun.orientation.y;
-    zpos = sun.orientation.z;
-    wpos = sun.orientation.w;
+    particleSystem.Stop();
   }
 
   // Update is called once per frame
   void Update()
   {
-    sun.orientation.Set(sun.orientation.x + 1, ypos, zpos, wpos);
-    if (sun.orientation.x > 360)
+    sun.transform.Rotate(0.02f,0,0, Space.Self);
+    particleSystem.transform.Rotate(0, 0, 0.02f, Space.Self);
+    rotation = rotation + 0.02f;
+    if (isNight && particleSystem.lights.light.intensity < 1)
     {
-      sun.orientation.Set(0, ypos, zpos, wpos);
+      
     }
+    if (rotation >= 170 && !isNight)
+    {
+      print("Night");
+      TransformToNight();
+    }
+
+    if (rotation >= 340 && isNight)
+    {
+      print("daytime");
+      TransformToDay();
+    }
+
+    if (rotation >= 360)
+    {
+      rotation = 0;
+    }
+  }
+
+  private void TransformToNight()
+  {
+    particleSystem.Play();
+    isNight = true;
+  }
+
+  private void TransformToDay()
+  {
+    particleSystem.Clear();
+    particleSystem.Stop();
+    isNight = false;
   }
 }
