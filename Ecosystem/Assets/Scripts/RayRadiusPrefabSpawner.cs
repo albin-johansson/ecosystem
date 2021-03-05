6673bc237e2508e2584241948ea3481177cd2969
SebastianHolm <54844314+SebastianHolm@users.noreply.github.com>
2021-03-05 13:37:18 +0100

    Fixed instantiating
    Now all instantiating of objects will be done in the given parent directory. For example the food spawners now has an additional field were the transform for carrot should be put so that the random generated carrots gets placed in the same place as the carrots that exist from the beginning.

--- Ecosystem/Assets/Scripts/RayRadiusPrefabSpawner.cs
+++ Ecosystem/Assets/Scripts/RayRadiusPrefabSpawner.cs
@@ -5,6 +5,7 @@ namespace Ecosystem
 // Will spawn a given prefab inside a radius if it hits something with a ground tag.
   public sealed class RayRadiusPrefabSpawner : MonoBehaviour
   {
+    [SerializeField] private Transform directory;
     [SerializeField] private GameObject prefab;
     [SerializeField] private float rate;
     [SerializeField] private float radius;
@@ -22,14 +23,14 @@ namespace Ecosystem
         var position = transform.position + distance * new Vector3(dir.x, 0, dir.y);
 
         if (!Physics.Raycast(position + new Vector3(0, transform.position.y + 5, 0), Vector3.down, out var hit,
-                200.0f))
+          200.0f))
         {
           return;
         }
 
         if (hit.transform.CompareTag("Ground"))
         {
-          Instantiate(prefab, hit.point, Quaternion.identity);
+          Instantiate(prefab, hit.point, Quaternion.identity, directory);
         }
       }
     }
