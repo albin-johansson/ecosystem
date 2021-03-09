3c3f944d3201833f248f040cac22d63fcdc07c71
Albin Johansson <albinoscar99@hotmail.com>
2021-03-09 16:52:37 +0100

    Improve how the SceneSelector opens scenes
    It now assumes that all our scenes adhere to the convention that it is located under "Scenes" in a folder with the same name as the scene. For example, "Scenes/ForestScene/ForestScene".

--- Ecosystem/Assets/Scripts/UI/SceneSelector.cs
+++ Ecosystem/Assets/Scripts/UI/SceneSelector.cs
@@ -5,20 +5,11 @@ namespace Ecosystem.UI
 {
   public sealed class SceneSelector : MonoBehaviour
   {
-    [SerializeField] private string button;
+    [SerializeField] private string scene;
+
     public void SelectScene()
     {
-      switch (button)
-      {
-        case "ForestScene":
-          SceneManager.LoadScene("Scenes/ForestScene/ForestScene");
-          break;
-        case "SampleScene":
-          SceneManager.LoadScene("Scenes/PrototypeScene/PrototypeScene");
-          break;
-        default:
-          break;
-      }
+      SceneManager.LoadScene($"Scenes/{scene}/{scene}");
     }
   }
 }
