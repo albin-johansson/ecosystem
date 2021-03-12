2e7ed665e98d7d5fb5f2bcc52d6a08b930146372
Karl Öqvist <karloq@student.chalmers.se>
2021-03-12 15:36:01 +0100

    Add basic object pool

---
+++ Ecosystem/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ecosystem
+{
+  public class ObjectPool : MonoBehaviour
+  {
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Queue<GameObject> animalPool = new Queue<GameObject>();
+    [SerializeField] private int poolStartSize;
+
+    private void Start()
+    {
+      for (var i = 0; i < poolStartSize; i++)
+      {
+        var animal = Instantiate(prefab);
+        animalPool.Enqueue(animal);
+        animal.SetActive(false);
+      }
+    }
+    public GameObject GetAnimal(){
+
+      if (animalPool.Count > 0)
+      {
+        var animal = animalPool.Dequeue();
+        return animal;
+      }
+      else
+      {
+        var animal = Instantiate(prefab);
+        return animal;
+      }
+    }
+    
+   public void ReturnAnimal(GameObject animal)
+    {
+      animalPool.Enqueue(animal);
+      animal.SetActive(false);
+    }
+  }
+}
