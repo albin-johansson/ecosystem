bfd9e08c9eae444e5e64ac1634233167625cb605
Braxen <oliver.k@live.se>
2021-03-12 15:29:49 +0100

    Fix: Some balancing issues

--- Ecosystem/Assets/Scripts/Genes/DeerGenome.cs
+++ Ecosystem/Assets/Scripts/Genes/DeerGenome.cs
@@ -5,15 +5,15 @@ namespace Ecosystem.Genes
 {
   public sealed class DeerGenome : AbstractGenome
   {
-    private static readonly Gene HungerRate = new Gene(1, 0.5f, 1.5f);
+    private static readonly Gene HungerRate = new Gene(0.5f, 0.1f, 1f);
     private static readonly Gene HungerThreshold = new Gene(5, 0, 10);
-    private static readonly Gene ThirstRate = new Gene(1, 0.5f, 1.5f);
+    private static readonly Gene ThirstRate = new Gene(0.5f, 0.1f, 1f);
     private static readonly Gene ThirstThreshold = new Gene(5, 0, 10);
     private static readonly Gene Vision = new Gene(1f, .5f, 1.5f);
     private static readonly Gene SpeedFactor = new Gene(1f, .5f, 1.5f);
     private static readonly Gene SizeFactor = new Gene(1, 0.5f, 1.5f);
     private static readonly Gene DesirabilityFactor = new Gene(0.5f, 0, 1);
-    private static readonly Gene GestationPeriod = new Gene(20, 10, 120);
+    private static readonly Gene GestationPeriod = new Gene(60, 20, 120);
     private static readonly Gene SexualMaturityTime = new Gene(20, 10, 120);
 
     public static readonly Dictionary<GeneType, Gene> DefaultGenes = new Dictionary<GeneType, Gene>
