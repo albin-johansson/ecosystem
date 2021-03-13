b8003676aa7653369fff123667489afcbd6e69da
SebastianHolm <54844314+SebastianHolm@users.noreply.github.com>
2021-03-13 14:11:04 +0100

    More fixes

--- Ecosystem/Assets/Scripts/UI/GraphicsSettings.cs
+++ Ecosystem/Assets/Scripts/UI/GraphicsSettings.cs
@@ -4,43 +4,32 @@ using UnityEngine.UI;
 
 namespace Ecosystem.UI
 {
-    public sealed class GraphicsSettings : MonoBehaviour
+  public sealed class GraphicsSettings : MonoBehaviour
+  {
+    [SerializeField] private GameObject settingsWindow;
+
+    public static int _antiAliasingMode;
+    public static bool _ambientOcclusionActive = true;
+    public static int _ambientOcclusionIntensity = 25;
+
+    public void SettingsMenu()
     {
-        [SerializeField] private Image settingsWindow;
-
-        private bool _windowIsOpen;
-        public static int _antiAliasingMode;
-        public static bool _ambientOcclusionActive = true;
-        public static int _ambientOcclusionIntensity = 25;
-        
-        public void SettingsMenu()
-        {
-            if (_windowIsOpen)
-            {
-                _windowIsOpen = false;
-                settingsWindow.gameObject.SetActive(false);
-            }
-            else
-            {
-                _windowIsOpen = true;
-                settingsWindow.gameObject.SetActive(true);
-            }
-        }
-
-        public void AntiAliasingOption()
-        {
-            _antiAliasingMode = gameObject.GetComponent<Dropdown>().value;
-        }
-
-        public void AmbientOcclusionActive()
-        {
-            _ambientOcclusionActive = !_ambientOcclusionActive;
-        }
-
-        public void AmbientOcclusionSlider()
-        {
-            _ambientOcclusionIntensity =  (int) Math.Round(100 * gameObject.GetComponent<Slider>().value);
-            print(_ambientOcclusionIntensity);
-        }
+      settingsWindow.SetActive(!settingsWindow.activeSelf);
     }
+
+    public void AntiAliasingOption()
+    {
+      _antiAliasingMode = gameObject.GetComponent<Dropdown>().value;
+    }
+
+    public void AmbientOcclusionActive()
+    {
+      _ambientOcclusionActive = !_ambientOcclusionActive;
+    }
+
+    public void AmbientOcclusionSlider()
+    {
+      _ambientOcclusionIntensity = (int) Math.Round(100 * gameObject.GetComponent<Slider>().value);
+    }
+  }
 }
--- Ecosystem/Assets/Scripts/UI/HandlePostProcessing.cs
+++ Ecosystem/Assets/Scripts/UI/HandlePostProcessing.cs
@@ -4,32 +4,33 @@ using UnityEngine.Rendering.PostProcessing;
 
 namespace Ecosystem.UI
 {
-    public class HandlePostProcessing : MonoBehaviour
+  public class HandlePostProcessing : MonoBehaviour
+  {
+    [SerializeField] private PostProcessVolume ppVolume;
+    [SerializeField] private PostProcessLayer ppLayer;
+
+    private void Start()
     {
-        [SerializeField] private PostProcessVolume ppVolume;
-        [SerializeField] private PostProcessLayer ppLayer;
-        
-        private void Start()
-        {
-            switch (GraphicsSettings._antiAliasingMode)
-            {
-                case 0:
-                    ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
-                    break;
-                case 1:
-                    ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.TemporalAntialiasing;
-                    break;
-                case 2:
-                    ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.FastApproximateAntialiasing;
-                    break;
-                case 3:
-                    ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.None;
-                    break;
-                default: break;
-            }
-            
-            ppVolume.profile.GetSetting<AmbientOcclusion>().active = GraphicsSettings._ambientOcclusionActive;
-            ppVolume.profile.GetSetting<AmbientOcclusion>().intensity.value = (float) GraphicsSettings._ambientOcclusionIntensity/100;
-        }
+      switch (GraphicsSettings._antiAliasingMode)
+      {
+        case 0:
+          ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
+          break;
+        case 1:
+          ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.TemporalAntialiasing;
+          break;
+        case 2:
+          ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.FastApproximateAntialiasing;
+          break;
+        case 3:
+          ppLayer.antialiasingMode = PostProcessLayer.Antialiasing.None;
+          break;
+        default: break;
+      }
+
+      ppVolume.profile.GetSetting<AmbientOcclusion>().active = GraphicsSettings._ambientOcclusionActive;
+      ppVolume.profile.GetSetting<AmbientOcclusion>().intensity.value =
+        (float) GraphicsSettings._ambientOcclusionIntensity / 100;
     }
+  }
 }
