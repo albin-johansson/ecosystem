5602d82df83144aeb12792a428f95029a8a62401
Albin Johansson <albinoscar99@hotmail.com>
2021-03-15 18:54:34 +0100

    Add NavTerrainSpawner

---
+++ Ecosystem/Assets/Scripts/ECS/NavTerrainSpawner.cs
@@ -0,0 +1,74 @@
+using Ecosystem.Authoring;
+using Reese.Nav;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Ecosystem.ECS
+{
+  public sealed class NavTerrainSpawner : MonoBehaviour
+  {
+    [SerializeField] private int amount = 100;
+    [SerializeField] private float3 spawnOffset = new float3(0, 1, 0);
+    [SerializeField] private float3 destination;
+
+    private Entity _prefab;
+
+    private static EntityManager EntityManager => World.DefaultGameObjectInjectionWorld.EntityManager;
+
+    private void Start()
+    {
+      _prefab = EntityManager.CreateEntityQuery(typeof(CylinderPrefab)).GetSingleton<CylinderPrefab>().Value;
+    }
+
+    private void Update()
+    {
+      if (Input.GetMouseButtonUp(0))
+      {
+        Spawn();
+      }
+    }
+
+    private void Spawn()
+    {
+      var entities = new NativeArray<Entity>(amount, Allocator.Temp);
+
+      EntityManager.Instantiate(_prefab, entities);
+
+      foreach (var entity in entities)
+      {
+        EntityManager.AddComponentData(entity, new NavAgent
+        {
+                TranslationSpeed = 20 + 5 * Random.value,
+                RotationSpeed = 0.3f,
+                TypeID = NavUtil.GetAgentType(NavConstants.HUMANOID),
+                Offset = new float3(0, 1, 0)
+        });
+
+        EntityManager.AddComponentData(entity, new Translation
+        {
+                Value = spawnOffset
+        });
+
+        EntityManager.AddComponent<LocalToWorld>(entity);
+        EntityManager.AddComponent<Parent>(entity);
+        EntityManager.AddComponent<LocalToParent>(entity);
+        EntityManager.AddComponent<NavNeedsSurface>(entity);
+        EntityManager.AddComponent<NavTerrainCapable>(entity);
+
+        EntityManager.AddComponentData(entity, new NavDestination
+        {
+                Teleport = false,
+                Tolerance = 10f,
+                CustomLerp = false,
+                WorldPoint = destination
+        });
+      }
+
+      entities.Dispose();
+    }
+  }
+}
