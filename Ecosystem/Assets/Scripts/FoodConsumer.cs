f480645860b53f17e7fcace32864cd0fc7f05cc2
Karl Öqvist <karloq@student.chalmers.se>
2021-03-17 11:02:09 +0100

    Consumed food returns to object pool

--- Ecosystem/Assets/Scripts/FoodConsumer.cs
+++ Ecosystem/Assets/Scripts/FoodConsumer.cs
@@ -1,9 +1,11 @@
 using System;
 using Ecosystem.Genes;
 using Ecosystem.Logging;
+using Ecosystem.Spawning;
 using Ecosystem.UI;
 using Ecosystem.Util;
 using UnityEngine;
+using Object = System.Object;
 
 namespace Ecosystem
 {
@@ -55,6 +57,11 @@ namespace Ecosystem
       if (Tags.IsFood(other.gameObject))
       {
         OnFoodEaten?.Invoke(other.gameObject);
+        var gameObjectTag = other.gameObject.tag;
+        if (ObjectPoolHandler.instance.isPoolValid(gameObjectTag))
+        {
+          ObjectPoolHandler.instance.ReturnToPool(gameObjectTag, other.gameObject);
+        }
         Destroy(other.gameObject);
         Hunger = 0;
       }
