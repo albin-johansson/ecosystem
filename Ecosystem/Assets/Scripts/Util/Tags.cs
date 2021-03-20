3ea7c4472ef52d757aa15319ac81da3255b2061e
mattiasHeinl <heinl@student.chalmers.se>
2021-03-20 02:24:12 +0100

    Fixed which objects are trackable

--- Ecosystem/Assets/Scripts/Util/Tags.cs
+++ Ecosystem/Assets/Scripts/Util/Tags.cs
@@ -14,6 +14,11 @@ namespace Ecosystem.Util
     public static bool IsPrey(GameObject animal) => animal.CompareTag("Rabbit") ||
                                                     animal.CompareTag("Deer");
 
+    public static bool IsAnimal(GameObject gameObject) => gameObject.CompareTag("Rabbit") ||
+                                                          gameObject.CompareTag("Deer") ||
+                                                          gameObject.CompareTag("Bear") ||
+                                                          gameObject.CompareTag("Wolf");
+
     /// <summary>
     ///   Indicates whether or not the supplied game object has a tag that indicates that it's a food item.
     /// </summary>
