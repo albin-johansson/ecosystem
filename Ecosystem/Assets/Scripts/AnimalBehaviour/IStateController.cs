d2bd9d149a764a8e2385f9a265f402c51ba9f2a9
Albin Johansson <albinoscar99@hotmail.com>
2021-03-22 10:05:32 +0100

    Update IStateController.cs

--- Ecosystem/Assets/Scripts/AnimalBehaviour/IStateController.cs
+++ Ecosystem/Assets/Scripts/AnimalBehaviour/IStateController.cs
@@ -5,9 +5,13 @@ namespace Ecosystem.AnimalBehaviour
   public interface IStateController
   {
     void Start();
+
     void Update();
+
     void OnTriggerEnter(Collider other);
+
     void OnTriggerExit(Collider other);
+
     void SwitchState(AnimalState state);
   }
 }
