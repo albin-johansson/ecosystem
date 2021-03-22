78a6ff5eabccbe0b4a9659379eebc65864d90b2d
Albin Johansson <albinoscar99@hotmail.com>
2021-03-22 10:05:28 +0100

    Update RabbitStateController.cs

--- Ecosystem/Assets/Scripts/AnimalBehaviour/RabbitStates/RabbitStateController.cs
+++ Ecosystem/Assets/Scripts/AnimalBehaviour/RabbitStates/RabbitStateController.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 namespace Ecosystem.AnimalBehaviour.RabbitStates
 {
-  public class RabbitStateController : AbstractStateController
-  {    
-    [SerializeField] private FoodConsumer consumer; 
+  public sealed class RabbitStateController : AbstractStateController
+  {
+    [SerializeField] private FoodConsumer consumer;
     [SerializeField] private WaterConsumer waterConsumer;
     [SerializeField] private MovementController movementController;
     [SerializeField] private EcoAnimationController animationController;
     [SerializeField] private MemoryController memoryController;
+
     private IAnimalState _idle;
     private IAnimalState _lookingForFood;
     private IAnimalState _lookingForWater;
@@ -16,63 +17,76 @@ namespace Ecosystem.AnimalBehaviour.RabbitStates
     private IAnimalState _runningTowardsWater;
     private IAnimalState _runningTowardsFood;
     private IAnimalState _fleeing;
-    private RabbitStateData stateData;
 
+    private RabbitStateData _stateData;
 
     public override void Start()
     {
-      stateData = new RabbitStateData(consumer, waterConsumer, movementController, animationController, memoryController);
-      _idle = RabbitStateFactory.CreateIdle(stateData);
-      _lookingForFood = RabbitStateFactory.CreateLookingForFood(stateData);
-      _lookingForWater = RabbitStateFactory.CreateLookingForWater(stateData);
-      _drinking = RabbitStateFactory.CreateDrinking(stateData);
-      _runningTowardsWater = RabbitStateFactory.CreateRunningTowardsWater(stateData);
-      _runningTowardsFood = RabbitStateFactory.CreateRunningTowardsFood(stateData);
-      _fleeing = RabbitStateFactory.CreateFleeing(stateData);
-      _state = _idle;
+      _stateData = new RabbitStateData
+      {
+              consumer = consumer,
+              animationController = animationController,
+              memoryController = memoryController,
+              movementController = movementController,
+              waterConsumer = waterConsumer
+      };
+
+      _idle = RabbitStateFactory.CreateIdle(_stateData);
+      _lookingForFood = RabbitStateFactory.CreateLookingForFood(_stateData);
+      _lookingForWater = RabbitStateFactory.CreateLookingForWater(_stateData);
+      _drinking = RabbitStateFactory.CreateDrinking(_stateData);
+      _runningTowardsWater = RabbitStateFactory.CreateRunningTowardsWater(_stateData);
+      _runningTowardsFood = RabbitStateFactory.CreateRunningTowardsFood(_stateData);
+      _fleeing = RabbitStateFactory.CreateFleeing(_stateData);
+      State = _idle;
 
       SwitchState(AnimalState.Idle);
     }
 
     public override void SwitchState(AnimalState state)
     {
-      var target = _state.End();
+      var target = State.End();
       switch (state)
-      { 
-        case  AnimalState.LookingForWater:
-          _state = _lookingForWater;
+      {
+        case AnimalState.LookingForWater:
+          State = _lookingForWater;
           break;
 
-        case  AnimalState.LookingForFood:
-          _state = _lookingForFood;
+        case AnimalState.LookingForFood:
+          State = _lookingForFood;
           break;
 
-        case  AnimalState.Idle:
-          _state = _idle;
+        case AnimalState.Idle:
+          State = _idle;
           break;
 
-        case  AnimalState.Fleeing:
-          _state = _fleeing;
-          break;
-          
-        case  AnimalState.Drinking:
-          _state = _drinking;
+        case AnimalState.Fleeing:
+          State = _fleeing;
           break;
 
-        case  AnimalState.RunningTowardsWater:
-          _state = _runningTowardsWater;
+        case AnimalState.Drinking:
+          State = _drinking;
           break;
 
-        case  AnimalState.RunningTowardsFood:
-          _state = _runningTowardsFood;
+        case AnimalState.RunningTowardsWater:
+          State = _runningTowardsWater;
           break;
 
+        case AnimalState.RunningTowardsFood:
+          State = _runningTowardsFood;
+          break;
+
+        case AnimalState.LookingForPrey:
+          break;
+
+        case AnimalState.ChasingPrey:
+          break;
+        
         default:
           break;
-      
       }
-      _state.Begin(target);
-    }
 
+      State.Begin(target);
+    }
   }
 }
