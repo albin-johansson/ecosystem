0bf47c554e0bc1ee7cb0224610b75c0485fd091a
Albin Johansson <albinoscar99@hotmail.com>
2021-03-23 11:38:27 +0100

    Add Idle and Roaming state components

---
+++ Ecosystem/Assets/Scripts/Components/States.cs
@@ -0,0 +1,15 @@
+using System;
+using Unity.Entities;
+
+namespace Ecosystem.Components
+{
+  [Serializable]
+  public struct Idle : IComponentData
+  {
+  }
+
+  [Serializable]
+  public struct Roaming : IComponentData
+  {
+  }
+}
