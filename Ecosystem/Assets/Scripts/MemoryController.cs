a0c729555517a43ea752407e309ad12425871adc
Karl Öqvist <karloq@student.chalmers.se>
2021-03-26 13:00:39 +0100

    Removed comment

--- Ecosystem/Assets/Scripts/MemoryController.cs
+++ Ecosystem/Assets/Scripts/MemoryController.cs
@@ -14,8 +14,7 @@ namespace Ecosystem
     {
       _memory = new List<GameObject>(Capacity);
     }
-
-    //Saves a game objects position and its desire to memory
+    
     public void SaveToMemory(GameObject other)
     {
       _memory.Insert(_nextMemoryLocation, (other.gameObject));
