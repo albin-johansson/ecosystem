4b46c4f3420dcd0635ead6a8dfcff0cbe86e4b9e
Albin Johansson <albinoscar99@hotmail.com>
2021-03-31 14:58:09 +0200

    Add InitialSimulationData component

---
+++ Ecosystem/Assets/Scripts/Components/InitialSimulationData.cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.Entities;
+
+namespace Ecosystem.Components
+{
+  [Serializable]
+  public struct InitialSimulationData : IComponentData
+  {
+    public int initialRabbitCount;
+    public int initialWolfCount;
+  }
+}
