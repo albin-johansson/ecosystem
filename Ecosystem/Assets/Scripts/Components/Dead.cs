3ae0444d49da5860b1ef038f5b785de8af65954e
Albin Johansson <albinoscar99@hotmail.com>
2021-04-02 14:17:25 +0200

    Split state components

--- Ecosystem/Assets/Scripts/Components/Dead.cs
+++ Ecosystem/Assets/Scripts/Components/Dead.cs
@@ -5,13 +5,9 @@ using Unity.Entities;
 namespace Ecosystem.Components
 {
   [Serializable]
-  public struct Roaming : IComponentData
-  {
-  }
-
-  [Serializable]
   public struct Dead : IComponentData
   {
+    public double when;
     public CauseOfDeath cause;
   }
 }
---
+++ Ecosystem/Assets/Scripts/Components/Roaming.cs
@@ -0,0 +1,10 @@
+using System;
+using Unity.Entities;
+
+namespace Ecosystem.Components
+{
+  [Serializable]
+  public struct Roaming : IComponentData
+  {
+  }
+}
