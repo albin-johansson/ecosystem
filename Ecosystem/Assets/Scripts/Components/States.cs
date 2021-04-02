09f24cdbf38f07eb9875d34dbe336ffab58f0544
Albin Johansson <albinoscar99@hotmail.com>
2021-04-02 14:15:52 +0200

    Update States.cs

--- Ecosystem/Assets/Scripts/Components/States.cs
+++ Ecosystem/Assets/Scripts/Components/States.cs
@@ -5,11 +5,6 @@ using Unity.Entities;
 namespace Ecosystem.Components
 {
   [Serializable]
-  public struct Idle : IComponentData
-  {
-  }
-
-  [Serializable]
   public struct Roaming : IComponentData
   {
   }
