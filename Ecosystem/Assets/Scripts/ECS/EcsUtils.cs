f349a0d2260217532abfa266f2fb4e5891c0ace2
Albin Johansson <albinoscar99@hotmail.com>
2021-04-02 14:14:41 +0200

    Update EcsUtils.cs

--- Ecosystem/Assets/Scripts/ECS/EcsUtils.cs
+++ Ecosystem/Assets/Scripts/ECS/EcsUtils.cs
@@ -1,5 +1,10 @@
+using Ecosystem.Components;
+using Ecosystem.Logging;
+using Reese.Nav;
+using Unity.Burst;
 using Unity.Entities;
 using UnityEngine.SceneManagement;
+using ParallelBuffer = Unity.Entities.EntityCommandBuffer.ParallelWriter;
 
 namespace Ecosystem.ECS
 {
@@ -8,6 +13,11 @@ namespace Ecosystem.ECS
   /// </summary>
   public static class EcsUtils
   {
+    public static void AddSingleton<T>(this EntityManager manager, T singleton) where T : struct, IComponentData
+    {
+      manager.AddComponentData(manager.CreateEntity(), singleton);
+    }
+
     // Extension function for entity managers to obtain a "singleton" component
     public static T GetSingleton<T>(this EntityManager manager) where T : struct, IComponentData
     {
@@ -18,6 +28,17 @@ namespace Ecosystem.ECS
     public static bool IsEcsCapable(Scene scene)
     {
       return scene.name == "ECSDemo";
+    }
+
+    [BurstCompile]
+    public static void Kill(ref ParallelBuffer buffer, int index, in Entity entity, double time, CauseOfDeath cause)
+    {
+      buffer.AddComponent(index, entity, new Dead
+      {
+              when = time,
+              cause = cause
+      });
+      buffer.AddComponent<NavStop>(index, entity);
     }
   }
 }
