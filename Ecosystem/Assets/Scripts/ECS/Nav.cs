e2b7d922329d3e6ff6fa0933a66679039dabb982
Albin Johansson <albinoscar99@hotmail.com>
2021-04-04 18:39:52 +0200

    Update Nav.cs

--- Ecosystem/Assets/Scripts/ECS/Nav.cs
+++ Ecosystem/Assets/Scripts/ECS/Nav.cs
@@ -2,6 +2,7 @@ using Ecosystem.Components;
 using Reese.Nav;
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using ParallelBuffer = Unity.Entities.EntityCommandBuffer.ParallelWriter;
 
 namespace Ecosystem.ECS
@@ -14,6 +15,18 @@ namespace Ecosystem.ECS
       buffer.RemoveComponent<NavFollow>(index, entity);
       buffer.AddComponent<NavStop>(index, entity);
       buffer.AddComponent<Roaming>(index, entity);
+    }
+
+    [BurstCompile]
+    public static void SetDestination(ref ParallelBuffer buffer, int index, in Entity entity, in float3 destination)
+    {
+      buffer.AddComponent(index, entity, new NavDestination
+      {
+              Teleport = false,
+              Tolerance = 2.0f,
+              CustomLerp = false,
+              WorldPoint = destination
+      });
     }
   }
 }
