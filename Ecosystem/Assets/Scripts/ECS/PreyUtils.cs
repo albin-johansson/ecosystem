a313e86ab2a3c1f75d3ea5f6f180d505e5724042
Albin Johansson <albinoscar99@hotmail.com>
2021-04-04 18:54:25 +0200

    Add PreyUtils

---
+++ Ecosystem/Assets/Scripts/ECS/PreyUtils.cs
@@ -0,0 +1,37 @@
+using Ecosystem.Components;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using ParallelBuffer = Unity.Entities.EntityCommandBuffer.ParallelWriter;
+
+namespace Ecosystem.ECS
+{
+  public static class PreyUtils
+  {
+    [BurstCompile]
+    public static void StopGoingForFoodAndRoam(ref ParallelBuffer buffer, int index, in Entity entity)
+    {
+      buffer.RemoveComponent<MovingTowardsFood>(index, entity);
+      buffer.AddComponent<Roaming>(index, entity);
+    }
+
+    [BurstCompile]
+    public static void RegisterConsumption(ref ParallelBuffer buffer, int index, double when)
+    {
+      var entity = buffer.CreateEntity(index);
+      buffer.AddComponent(index, entity, new Consumed
+      {
+              when = when
+      });
+    }
+
+    [BurstCompile]
+    public static void ConsumeFood(ref ParallelBuffer buffer, int index, in Entity entity, in Hunger hunger)
+    {
+      var newHunger = hunger;
+      newHunger.value -= 0.75f * newHunger.max;
+      newHunger.value = math.clamp(newHunger.value, 0, newHunger.max);
+      buffer.SetComponent(index, entity, newHunger);
+    }
+  }
+}
