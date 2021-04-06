a8c9e1b1906af86af0fd6f8e8d30ba846337a194
Braxen <oliver.k@live.se>
2021-04-06 16:05:45 +0200

    Fix: Now uses pool. Also removes the meat after food has decayed to 0

--- Ecosystem/Assets/Scripts/NutritionController.cs
+++ Ecosystem/Assets/Scripts/NutritionController.cs
@@ -6,51 +6,60 @@ using UnityEngine;
 
 namespace Ecosystem
 {
-    public class NutritionController : MonoBehaviour
+  public class NutritionController : MonoBehaviour
+  {
+    public double nutritionalValue;
+    [SerializeField] private string keyToPool;
+
+    public delegate void FoodEatenEvent(GameObject food);
+
+    /// <summary>
+    /// This event is emitted every time a food resource is consumed.
+    /// </summary>
+    public static event FoodEatenEvent OnFoodEaten;
+
+    public void Update()
     {
-        public double nutritionalValue;
-        [SerializeField] private string keyToPool;
-
-        public delegate void FoodEatenEvent(GameObject food);
-
-        /// <summary>
-        /// This event is emitted every time a food resource is consumed.
-        /// </summary>
-        public static event FoodEatenEvent OnFoodEaten;
-
-        public void Update()
+      // Simulates nutritional decay.
+      if (nutritionalValue > 0)
+      {
+        nutritionalValue = (double) Mathf.Max((float) 0, (float) nutritionalValue - Time.deltaTime * 0.1f);
+        if (nutritionalValue == 0)
         {
-            // Simulates nutritional decay.
-            if (nutritionalValue > 0)
-            {
-                nutritionalValue = (double) Mathf.Max((float) 0, (float) nutritionalValue - Time.deltaTime * 0.1f);
-            }
+          ReturnToPool();
         }
-        public double Consume(Double hunger)
-        {
-            if (hunger < nutritionalValue)
-            {
-                nutritionalValue -= hunger;
-                return hunger;
-            }
-            
-            OnFoodEaten?.Invoke(gameObject);
-            var gameObjectTag = gameObject.tag;
-            if (ObjectPoolHandler.instance.isPoolValid(keyToPool))
-            {
-                ObjectPoolHandler.instance.ReturnToPool(gameObjectTag, gameObject);
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-            
-            return nutritionalValue;
-        }
-
-        public void SetNutrition(double value)
-        {
-            nutritionalValue = value;
-        }
+      }
     }
+
+    public double Consume(Double hunger)
+    {
+      if (hunger < nutritionalValue)
+      {
+        nutritionalValue -= hunger;
+        return hunger;
+      }
+
+      OnFoodEaten?.Invoke(gameObject);
+      ReturnToPool();
+
+      return nutritionalValue;
+    }
+
+    public void SetNutrition(double value)
+    {
+      nutritionalValue = value;
+    }
+
+    private void ReturnToPool()
+    {
+      if (ObjectPoolHandler.instance.isPoolValid(keyToPool))
+      {
+        ObjectPoolHandler.instance.ReturnToPool(keyToPool, gameObject);
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
+    }
+  }
 }
