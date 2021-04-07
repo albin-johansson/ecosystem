d66eb55ff1dd2068df9ba50bd6276e2188e2a00c
Albin Johansson <albinoscar99@hotmail.com>
2021-04-07 14:44:34 +0200

    Update EcsUtils.cs

--- Ecosystem/Assets/Scripts/ECS/EcsUtils.cs
+++ Ecosystem/Assets/Scripts/ECS/EcsUtils.cs
@@ -13,23 +13,47 @@ namespace Ecosystem.ECS
   /// </summary>
   public static class EcsUtils
   {
+    /// <summary>
+    ///   Adds a singleton component to the entity manager.
+    /// </summary>
+    /// <param name="manager">the source entity manager.</param>
+    /// <param name="singleton">the component that will be added to the entity manager as a singleton.</param>
+    /// <typeparam name="T">the type of the singleton component.</typeparam>
     public static void AddSingleton<T>(this EntityManager manager, T singleton) where T : struct, IComponentData
     {
       manager.AddComponentData(manager.CreateEntity(), singleton);
     }
 
-    // Extension function for entity managers to obtain a "singleton" component
+    /// <summary>
+    ///   Returns a singleton component, i.e. a component that there only exists a single instance of in the the entity
+    ///   manager.
+    /// </summary>
+    /// <param name="manager">the source entity manager.</param>
+    /// <typeparam name="T">the type of the singleton component.</typeparam>
+    /// <returns>the singleton component.</returns>
     public static T GetSingleton<T>(this EntityManager manager) where T : struct, IComponentData
     {
       return manager.CreateEntityQuery(typeof(T)).GetSingleton<T>();
     }
 
-    // Simple utility for checking if a scene is known to support the ECS framework
+    /// <summary>
+    ///   Indicates whether or not a scene supports the ECS simulation framework.
+    /// </summary>
+    /// <param name="scene">the scene that will be checked.</param>
+    /// <returns><c>true</c> if the scene supports the ECS framework; <c>false</c> otherwise.</returns>
     public static bool IsEcsCapable(Scene scene)
     {
       return scene.name == "ECSDemo";
     }
 
+    /// <summary>
+    ///   Marks an animal entity as dead and stops any potential movement of the animal.
+    /// </summary>
+    /// <param name="buffer">the parallel entity command buffer that will be used.</param>
+    /// <param name="index">the index associated with the operation, should be <c>entityInQueryIndex</c>.</param>
+    /// <param name="entity">the entity that will be killed.</param>
+    /// <param name="time">the time point that will be associated with the death of the entity.</param>
+    /// <param name="cause">the reason that the animal died.</param>
     [BurstCompile]
     public static void Kill(ref ParallelBuffer buffer, int index, in Entity entity, double time, CauseOfDeath cause)
     {
