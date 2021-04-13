0dfceb45c3305aba734302b2a1fc81c956c30463
Albin Johansson <albinoscar99@hotmail.com>
2021-04-13 13:56:18 +0200

    Fix layer masks

--- Ecosystem/Assets/Scripts/Util/Layers.cs
+++ Ecosystem/Assets/Scripts/Util/Layers.cs
@@ -4,13 +4,16 @@ namespace Ecosystem.Util
 {
   public static class Layers
   {
-    public static readonly int AnimalMask = LayerMask.GetMask("Bear", "Wolf", "Prey");
     public static readonly int RabbitMask = LayerMask.GetMask("Rabbit");
     public static readonly int DeerMask = LayerMask.GetMask("Deer");
     public static readonly int WolfMask = LayerMask.GetMask("Wolf");
     public static readonly int BearMask = LayerMask.GetMask("Bear");
-    public static readonly int PredatorMask = LayerMask.GetMask("Wolf", "Bear");
-    public static readonly int PreyMask = LayerMask.GetMask("Prey");
+
+    public static readonly int PredatorMask = WolfMask | BearMask;
+    public static readonly int PreyMask = RabbitMask | DeerMask;
+
+    public static readonly int AnimalMask = PredatorMask | PreyMask;
+
     public static readonly int WaterMask = LayerMask.GetMask("Water");
     public static readonly int FoodMask = LayerMask.GetMask("Food");
   }
