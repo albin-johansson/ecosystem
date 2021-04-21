c4522bec147d5f362be5af83f8634f39f8747606
SebastianHolm <54844314+SebastianHolm@users.noreply.github.com>
2021-04-21 14:40:46 +0200

    Changed some values

--- Ecosystem/Assets/Scripts/Genes/RabbitGenome.cs
+++ Ecosystem/Assets/Scripts/Genes/RabbitGenome.cs
@@ -10,19 +10,19 @@ namespace Ecosystem.Genes
     public static readonly Dictionary<GeneType, Preset> DefaultSet = new Dictionary<GeneType, Preset>()
     {
       {GeneType.HungerRate, new Preset(0.5f, 1, new[] {0.5f, 0.75f, 1f})},
-      {GeneType.HungerThreshold, new Preset(5, 15, new[] {5f, 10f, 15f})},
+      {GeneType.HungerThreshold, new Preset(10, 20, new[] {10f, 15f, 20f})},
       {GeneType.ThirstRate, new Preset(0.3f, 1, new[] {0.1f, 0.5f, 1f})},
       {GeneType.ThirstThreshold, new Preset(5, 15, new[] {5f, 10f, 15f})},
       {GeneType.Vision, new Preset(5, 10, new[] {5f, 6f, 7.5f, 9f, 10f})},
       {GeneType.Speed, new Preset(2, 4, new[] {2f, 3f, 4f})},
       {GeneType.GestationPeriod, new Preset(10, 120, new[] {12f, 20f, 50f, 70f, 90f, 110f})},
-      {GeneType.SexualMaturityTime, new Preset(10, 150, new[] {20f, 50f, 90f, 140f})}
+      {GeneType.SexualMaturityTime, new Preset(10, 50, new[] {10f, 20f, 30f, 40f, 50f})}
     };
 
     public static readonly Dictionary<GeneType, Preset> DefaultSingular = new Dictionary<GeneType, Preset>()
     {
       {GeneType.HungerRate, new Preset(0, 1, new[] {0.5f})},
-      {GeneType.HungerThreshold, new Preset(5, 15, new[] {10f})},
+      {GeneType.HungerThreshold, new Preset(10, 20, new[] {15f})},
       {GeneType.ThirstRate, new Preset(0.3f, 1, new[] {0.5f})},
       {GeneType.ThirstThreshold, new Preset(5, 15, new[] {10f})},
       {GeneType.Vision, new Preset(5, 10, new[] {7.5f})},
