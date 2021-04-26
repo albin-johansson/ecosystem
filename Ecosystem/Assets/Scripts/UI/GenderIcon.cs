6632c29f8d62e57c08140017ffbfc200ccb65297
Karl Öqvist <karloq@student.chalmers.se>
2021-04-26 16:29:30 +0200

    add gender icon script

---
+++ Ecosystem/Assets/Scripts/UI/GenderIcon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ecosystem.UI
+{
+  public sealed class GenderIcon : MonoBehaviour
+  {
+    private Sprite _sprite;
+    
+    public void SetGenderIcon(bool isMale)
+    {
+      var image = gameObject.GetComponent<Image>();
+      if (isMale)
+      {
+        _sprite =  Resources.Load <Sprite>("Sprites/male");
+        image.sprite = _sprite;
+      }
+      else
+      {
+        _sprite =  Resources.Load <Sprite>("Sprites/female");
+        image.sprite = _sprite;
+      }
+    }
+  }
+}
