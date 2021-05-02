e6a85dd1b4018d1234daecd75b376590ed3c6012
EdwinHolst <43250892+EdwinHolst@users.noreply.github.com>
2021-05-03 00:53:27 +0200

    balance genome values

--- Ecosystem/Assets/Scripts/Genes/BearGenome.cs
+++ Ecosystem/Assets/Scripts/Genes/BearGenome.cs
@@ -9,12 +9,12 @@ namespace Ecosystem.Genes
 
     public static readonly Dictionary<GeneType, Preset> DefaultSet = new Dictionary<GeneType, Preset>()
     {
-      {GeneType.HungerRate, new Preset(0.2f, 0.5f, new[] {0.2f, 0.3f, 0.4f, 0.5f})},
+      {GeneType.HungerRate, new Preset(0.2f, 0.5f, new[] {0.3f})},
       {GeneType.HungerThreshold, new Preset(30, 40, new[] {30f, 35f, 40f})},
-      {GeneType.ThirstRate, new Preset(0.3f, 0.5f, new[] {0.3f, 0.4f, 0.5f})},
+      {GeneType.ThirstRate, new Preset(0.3f, 0.5f, new[] {0.3f})},
       {GeneType.ThirstThreshold, new Preset(25, 40, new[] {25f, 35f, 40f})},
-      {GeneType.Vision, new Preset(10, 15, new[] {10f, 11f, 12.5f, 14f, 15f})},
-      {GeneType.Speed, new Preset(2, 4, new[] {2f, 3f, 4f})},
+      {GeneType.Vision, new Preset(5, 15, new[] {5f, 7f, 9f, 11f})},
+      {GeneType.Speed, new Preset(2, 4, new[] {2f, 3f})},
       {GeneType.GestationPeriod, new Preset(20, 120, new[] {20f, 60f, 90f, 120f})},
       {GeneType.SexualMaturityTime, new Preset(10, 50, new[] {10f, 20f, 30f, 40f, 50f})}
     };
