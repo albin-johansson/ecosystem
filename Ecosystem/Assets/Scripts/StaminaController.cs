58d37e5b10e024f9e2693a1892f4ba88b9a1fe2c
SebastianHolm <54844314+SebastianHolm@users.noreply.github.com>
2021-05-03 10:23:37 +0200

    Change stamina controller to vary between animals

--- Ecosystem/Assets/Scripts/StaminaController.cs
+++ Ecosystem/Assets/Scripts/StaminaController.cs
@@ -1,5 +1,6 @@
 using Ecosystem.Genes;
 using Ecosystem.UI;
+using Ecosystem.Util;
 using UnityEngine;
 
 namespace Ecosystem
@@ -12,6 +13,7 @@ namespace Ecosystem
 
     private const float ExhaustionMultiplier = 5;
     private const float BaseRunningLevel = 20;
+    private float _multiplier = 1;
 
     public bool IsRunning { get; set; }
 
@@ -22,18 +24,26 @@ namespace Ecosystem
       Exhaustion = 50;
       staminaBar.SetMaxValue(maxStamina);
       staminaBar.SetValue(Exhaustion);
+      if (genome.GetTag().Equals("Rabbit"))
+      {
+        _multiplier = 4;
+      }
+      else if (genome.GetTag().Equals("Deer"))
+      {
+        _multiplier = 2;
+      }
     }
 
     private void Update()
     {
       if (IsRunning)
       {
-        Exhaustion += ExhaustionMultiplier * genome.GetSpeed().Value * Time.deltaTime;
+        Exhaustion += ExhaustionMultiplier * genome.GetSpeed().Value * Time.deltaTime / _multiplier;
         staminaBar.SetValue(Exhaustion);
       }
       else if (Exhaustion > 0)
       {
-        Exhaustion -= genome.Metabolism * Time.deltaTime;
+        Exhaustion -= genome.Metabolism * Time.deltaTime * _multiplier;
         staminaBar.SetValue(Exhaustion);
       }
 
