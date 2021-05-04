378d9488284b191796c80cdc175e141d2d67c782
SebastianHolm <54844314+SebastianHolm@users.noreply.github.com>
2021-05-04 09:29:23 +0200

    Change overall

--- Ecosystem/Assets/Scripts/UI/ExitMenu.cs
+++ Ecosystem/Assets/Scripts/UI/ExitMenu.cs
@@ -1,28 +1,12 @@
-using Ecosystem.Logging;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Ecosystem.UI
 {
     public class ExitMenu : MonoBehaviour
     {
-        [SerializeField] private LoggingManager loggingManager;
-        [SerializeField] private string currentScene;
         public void CloseExitMenu()
         {
             gameObject.SetActive(false);
-        }
-
-        public void ReturnToMainMenu()
-        {
-            loggingManager.SceneExit();
-            SceneManager.LoadScene("MainMenu");
-        }
-
-        public void RestartScene()
-        {
-            loggingManager.SceneExit();
-            SceneManager.LoadScene(currentScene);
         }
 
         public void Exit()
