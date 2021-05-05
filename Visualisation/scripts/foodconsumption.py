9e3f52c6ee2941692933155e27ab15f10f8cd3f9
Karl Öqvist <karloq@student.chalmers.se>
2021-05-05 08:50:14 +0200

    Resolve PR-comments

--- Visualisation/scripts/foodconsumption.py
+++ Visualisation/scripts/foodconsumption.py
@@ -12,7 +12,7 @@ from logdata import *
 def visualise_food_consumption(data: LogData, directory: Path):
   """
   Produces a plot of how the food availability changed over the course
-  of the simulation, along with the amount of food consumers.
+  of the simulation.
 
   :param data: the simulation data to read from.
   :param directory: the directory to which the plot will be saved.
@@ -20,17 +20,14 @@ def visualise_food_consumption(data: LogData, directory: Path):
 
   figure, axes = plot.subplots()
 
-  consumer_history = get_population_history(data, ["Rabbit", "Deer"], data.initial_prey_count())
   food_history = get_food_history(data)
 
-  """axes.plot(consumer_history.keys(), consumer_history.values(), label="Consumers", color="green", **{"ls": "-"})"""
   axes.plot(food_history.keys(), food_history.values(), label="Food", color="blue", **{"ls": "--"})
 
   axes.legend(loc="upper left")
   axes.set_xlim(0, data.duration_secs())
   axes.set_xlabel("Time (seconds)")
   axes.set_ylabel("Amount")
-  """axes.set_title("Food availability and consumer population")"""
   axes.set_title("Food availability")
 
   plot.savefig(directory / Path("food_consumption.png"))
